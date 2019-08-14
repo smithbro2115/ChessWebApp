d24718f33f3b133e951b69db40cecfa9959c2047
Josh <smithbro2115@gmail.com>
2019-08-14 17:57:23 -0500

    - Started adding posts

--- chess_web_app/chess/urls.py
+++ chess_web_app/chess/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='chess-home'),
+    path('move_piece/', views.move_piece, name='chess-move-piece')
 ]
--- chess_web_app/chess/views.py
+++ chess_web_app/chess/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'chess/home.html')
+
+
+def move_piece(request):
+    if request.method == 'POST':
+        print(request.POST['newPos'])
+        if 'newPos' in request.POST:
+            newPos = request.POST['newPos']
+            # doSomething with pieFact here...
+            return HttpResponse('success')  # if everything is OK
+        # nothing went well
+    return HttpResponse('FAIL!!!!!')
