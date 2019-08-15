fed2f0c1e71416023885cbe5eef666d88eebebad
Joshua <smithbro2115@gmail.com>
2019-08-15 10:09:13 -0500

    - Working on making everything classes

--- chess_web_app/chess/views.py
+++ chess_web_app/chess/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
     return render(request, 'chess/home.html')
 
 
+@csrf_exempt
 def move_piece(request):
     if request.method == 'POST':
-        print(request.POST['newPos'])
+        print(request.POST)
         if 'newPos' in request.POST:
             newPos = request.POST['newPos']
-            # doSomething with pieFact here...
-            return HttpResponse('success')  # if everything is OK
-        # nothing went well
+            return HttpResponse('success')
     return HttpResponse('FAIL!!!!!')
